b694094707ad35477a027260cb5cb1629cfb7852
animestopbus-ctrl <animestopbus@gmail.com>
2026-02-12 09:11:55 +0530

    Update __int__.py

--- db/__int__.py
+++ db/__int__.py
@@ -1,3 +1,23 @@
-"""Handlers Package for LastPerson07Bot"""
+"""Database Package for LastPerson07Bot"""
 
-# This file intentionally left empty
+# Explicit imports to avoid circular import issues
+from .client import lastperson07_db_client
+from .queries import lastperson07_queries
+from .models import (
+    LastPerson07User, LastPerson07Schedule, LastPerson07ApiUrl,
+    LastPerson07BotSettings, LastPerson07WallpaperData,
+    UserTier, ScheduleInterval, UserStatus
+)
+
+__all__ = [
+    "lastperson07_db_client",
+    "lastperson07_queries", 
+    "LastPerson07User",
+    "LastPerson07Schedule", 
+    "LastPerson07ApiUrl",
+    "LastPerson07BotSettings",
+    "LastPerson07WallpaperData",
+    "UserTier",
+    "ScheduleInterval",
+    "UserStatus"
+]
